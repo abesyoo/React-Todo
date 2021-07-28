b6f3c20ed308e19b3e27a5fd0101a639060379b8
abesyoo <84064125+abesyoo@users.noreply.github.com>
2021-07-28 22:18:20 +0900

    Add files via upload

---
+++ src/components/InputTodo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const style ={backgroundColor: '#00ffff',
+    width: '400px',
+    height: '30px',
+    padding: '8px',
+    margin: '8px',
+    borderRadius: '8px'
+}
+
+export const InputTodo = (props) => {
+    const{todoText,onChange,onClick} = props;
+  return(
+    <div style={style}>
+    <input
+      placeholder="TODOを入力"
+      value={todoText}
+      onChange={onChange}
+    />
+    <button onClick={onClick}>追加</button>
+  </div>
+
+  )
+};
